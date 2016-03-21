685f0bc3b941a135bd26413c1502dc68a0e250fb
semenkravchenko <semen.kravchenko@gmail.com>
2016-03-21 19:18:50 +0300

    Fixed film_searching test

--- php4dvd/test_film_searching.py
+++ php4dvd/test_film_searching.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import *
 import unittest, time, re
 
 class Search(unittest.TestCase):
@@ -20,6 +22,7 @@ class Search(unittest.TestCase):
         class NotExistedFilmException(Exception): pass
 
         search_target = u"Криминальное чтиво"
+        wait = WebDriverWait(self.driver, 3)
 
         driver = self.driver
         driver.get(self.base_url + "/php4dvd/")
@@ -32,19 +35,23 @@ class Search(unittest.TestCase):
         driver.find_element_by_name("password").send_keys("admin")
         driver.find_element_by_name("submit").click()
 
+        random_film = driver.find_element_by_class_name("title")
+
         searchbox = driver.find_element_by_class_name("inputDefault")
         searchbox.clear()
         searchbox.send_keys(search_target)
         searchbox.send_keys(Keys.ENTER)
 
+        wait.until(invisibility_of_element_located((By.NAME, random_film)))
+
         time.sleep(1)
 
-        all_films = driver.find_elements_by_class_name("title")
+        films_after_search = driver.find_elements_by_class_name("title")
 
-        if len(all_films) == 0:
+        if len(films_after_search) == 0:
             raise NotExistedFilmException("Film was not found")
 
-        for film in all_films:
+        for film in films_after_search:
 
             if film.get_attribute("textContent") == search_target:
                 print "We have found film with title: ", film.get_attribute("textContent")
@@ -57,6 +64,7 @@ class Search(unittest.TestCase):
         class NotExistedFilmException(Exception): pass
 
         search_target = u"Красная шапочка"
+        wait = WebDriverWait(self.driver, 3)
 
         driver = self.driver
         driver.get(self.base_url + "/php4dvd/")
@@ -69,17 +77,21 @@ class Search(unittest.TestCase):
         driver.find_element_by_name("password").send_keys("admin")
         driver.find_element_by_name("submit").click()
 
+        random_film = driver.find_element_by_class_name("title")
+
         searchbox = driver.find_element_by_class_name("inputDefault")
         searchbox.clear()
         searchbox.send_keys(search_target)
         searchbox.send_keys(Keys.ENTER)
 
+        wait.until(invisibility_of_element_located((By.NAME, random_film)))
+
         time.sleep(1)
 
-        all_films = driver.find_elements_by_class_name("title")
+        films_after_search = driver.find_elements_by_class_name("title")
 
-        if len(all_films) > 0:
-            for film in all_films:
+        if len(films_after_search) > 0:
+            for film in films_after_search:
 
                 if film.get_attribute("textContent") == search_target:
                     print "We have found film with title: ", film.get_attribute("textContent")
