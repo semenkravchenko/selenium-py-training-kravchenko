02be5c07a400d3e2a948e2b8228a7c0a069a7d3f
semenkravchenko <semen.kravchenko@gmail.com>
2016-03-23 16:47:58 +0300

    Fixed film_adding and film_removing tests

--- php4dvd/test_film_adding.py
+++ php4dvd/test_film_adding.py
@@ -43,6 +43,237 @@ class Adding(unittest.TestCase):
 
         time.sleep(1)
 
+    def sender(self, element, keys):
+        element.clear()
+        element.send_keys(keys)
+        element.send_keys(Keys.TAB)
+
+    def test_film_adding_imbdid_field(self):
+
+        class BadRequiredFieldException(Exception):pass
+
+        driver = self.driver
+        driver.get(self.base_url + "/php4dvd/")
+        driver.implicitly_wait(3)
+
+        #login
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys("admin")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("admin")
+        driver.find_element_by_name("submit").click()
+
+        #film adding
+        driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
+
+        imbdid = driver.find_element_by_name("imdbid")
+
+        #case 1
+        self.sender(imbdid, "666")
+
+        if str(imbdid.get_attribute("class")) != "digits":
+            raise BadRequiredFieldException("Required field \"imbdid\" was filled bad")
+
+        #case 2
+        self.sender(imbdid, "-1")
+
+        if str(imbdid.get_attribute("class")) == "digits":
+            raise BadRequiredFieldException("Required field \"imbdid\" was filled bad")
+
+        #case 3
+        self.sender(imbdid, "not a number")
+
+        if str(imbdid.get_attribute("class")) == "digits":
+            raise BadRequiredFieldException("Required field \"imbdid\" was filled bad")
+
+        #case 4
+        self.sender(imbdid, "")
+
+        if str(imbdid.get_attribute("class")) != "digits":
+            raise BadRequiredFieldException("Required field \"imbdid\" was filled bad")
+
+    def test_film_adding_year_field(self):
+
+        class BadRequiredFieldException(Exception):pass
+
+        driver = self.driver
+        driver.get(self.base_url + "/php4dvd/")
+        driver.implicitly_wait(3)
+
+        #login
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys("admin")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("admin")
+        driver.find_element_by_name("submit").click()
+
+        #film adding
+        driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
+
+        year = driver.find_element_by_name("year")
+
+        #case 1
+        self.sender(year, "666")
+
+        if str(year.get_attribute("class")) != "required digits":
+            raise BadRequiredFieldException("Required field \"year\" was filled bad")
+
+        #case 2
+        self.sender(year, "-1")
+
+        if str(year.get_attribute("class")) == "required digits":
+            raise BadRequiredFieldException("Required field \"year\" was filled bad")
+
+        #case 3
+        self.sender(year, "not a number")
+
+        if str(year.get_attribute("class")) == "required digits":
+            raise BadRequiredFieldException("Required field \"year\" was filled bad")
+
+        #case 4
+        self.sender(year, "")
+
+        if str(year.get_attribute("class")) != "required digits error":
+            raise BadRequiredFieldException("Required field \"year\" was filled bad")
+
+    def test_film_adding_duration_field(self):
+
+        class BadRequiredFieldException(Exception):pass
+
+        driver = self.driver
+        driver.get(self.base_url + "/php4dvd/")
+        driver.implicitly_wait(3)
+
+        #login
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys("admin")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("admin")
+        driver.find_element_by_name("submit").click()
+
+        #film adding
+        driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
+
+        duration = driver.find_element_by_name("duration")
+
+        #case 1
+        self.sender(duration, "666")
+
+        if str(duration.get_attribute("class")) != "digits":
+            raise BadRequiredFieldException("Required field \"duration\" was filled bad")
+
+        #case 2
+        self.sender(duration, "-1")
+
+        if str(duration.get_attribute("class")) == "digits":
+            raise BadRequiredFieldException("Required field \"duration\" was filled bad")
+
+        #case 3
+        self.sender(duration, "not a number")
+
+        if str(duration.get_attribute("class")) == "digits":
+            raise BadRequiredFieldException("Required field \"duration\" was filled bad")
+
+        #case 4
+        self.sender(duration, "")
+
+        if str(duration.get_attribute("class")) != "digits":
+            raise BadRequiredFieldException("Required field \"duration\" was filled bad")
+
+    def test_film_adding_rating_field(self):
+
+        class BadRequiredFieldException(Exception):pass
+
+        driver = self.driver
+        driver.get(self.base_url + "/php4dvd/")
+        driver.implicitly_wait(3)
+
+        #login
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys("admin")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("admin")
+        driver.find_element_by_name("submit").click()
+
+        #film adding
+        driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
+
+        rating = driver.find_element_by_name("rating")
+
+        #case 1
+        self.sender(rating, "666")
+
+        if str(rating.get_attribute("class")) != "number":
+            raise BadRequiredFieldException("Required field \"rating\" was filled bad")
+
+        #case 2
+        self.sender(rating, "-1")
+
+        if str(rating.get_attribute("class")) != "number":
+            raise BadRequiredFieldException("Required field \"rating\" was filled bad")
+
+        #case 3
+        self.sender(rating, "not a number")
+
+        if str(rating.get_attribute("class")) == "number":
+            raise BadRequiredFieldException("Required field \"rating\" was filled bad")
+
+        #case 4
+        self.sender(rating, "")
+
+        if str(rating.get_attribute("class")) != "number":
+            raise BadRequiredFieldException("Required field \"rating\" was filled bad")
+
+    def test_film_adding_trailer_field(self):
+
+        class BadRequiredFieldException(Exception):pass
+
+        driver = self.driver
+        driver.get(self.base_url + "/php4dvd/")
+        driver.implicitly_wait(3)
+
+        #login
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys("admin")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("admin")
+        driver.find_element_by_name("submit").click()
+
+        #film adding
+        driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
+
+        trailer = driver.find_element_by_name("trailer")
+
+        #case 1
+        self.sender(trailer, "666")
+
+        if str(trailer.get_attribute("class")) == "url":
+            raise BadRequiredFieldException("Required field \"trailer\" was filled bad")
+
+        #case 2
+        self.sender(trailer, "-1")
+
+        if str(trailer.get_attribute("class")) == "url":
+            raise BadRequiredFieldException("Required field \"trailer\" was filled bad")
+
+        #case 3
+        self.sender(trailer, "some text")
+
+        if str(trailer.get_attribute("class")) == "url":
+            raise BadRequiredFieldException("Required field \"trailer\" was filled bad")
+
+        #case 4
+        self.sender(trailer, "")
+
+        if str(trailer.get_attribute("class")) != "url":
+            raise BadRequiredFieldException("Required field \"trailer\" was filled bad")
+
+        #case 5
+        self.sender(trailer, "http://www.ru")
+
+        if str(trailer.get_attribute("class")) != "url":
+            raise BadRequiredFieldException("Required field \"trailer\" was filled bad")
+
     def test_film_adding_full_data(self):
 
         class BadRequiredFieldException(Exception):pass
@@ -62,187 +293,104 @@ class Adding(unittest.TestCase):
         driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
 
         imbdid = driver.find_element_by_name("imdbid")
-        imbdid.clear()
-        imbdid.send_keys("666")
-        imbdid.send_keys(Keys.TAB)
+
+        self.sender(imbdid, "666")
 
         if str(imbdid.get_attribute("class")) != "digits":
             raise BadRequiredFieldException("Required field \"imbdid\" was filled bad")
 
         name = driver.find_element_by_name("name")
-        name.clear()
-        name.send_keys("Test Film Full Data")
+
+        self.sender(name, "Test Film Full Data")
 
         aka = driver.find_element_by_name("aka")
-        aka.clear()
-        aka.send_keys("Scary Test Film Full Data")
+
+        self.sender(aka, "Scary Test Film Full Data")
 
         year = driver.find_element_by_name("year")
-        year.clear()
-        year.send_keys("1937")
-        year.send_keys(Keys.TAB)
+
+        self.sender(year, "1937")
 
         if str(year.get_attribute("class")) != "required digits":
             raise BadRequiredFieldException("Required field \"year\" was filled bad")
 
         duration = driver.find_element_by_name("duration")
-        duration.clear()
-        duration.send_keys("13")
-        duration.send_keys(Keys.TAB)
+
+        self.sender(duration, "13")
 
         if str(duration.get_attribute("class")) != "digits":
             raise BadRequiredFieldException("Required field \"duration\" was filled bad")
 
         rating = driver.find_element_by_name("rating")
-        rating.clear()
-        rating.send_keys("777")
-        rating.send_keys(Keys.TAB)
+
+        self.sender(rating, "777")
 
         if str(rating.get_attribute("class")) != "number":
             raise BadRequiredFieldException("Required field \"rating\" was filled bad")
 
         format = driver.find_element_by_name("format")
-        format.clear()
-        format.send_keys("VHS")
-        format.send_keys(Keys.TAB)
+
+        self.sender(format, "VHS")
 
         if str(format.get_attribute("class")) != "required ui-autocomplete-input":
             raise BadRequiredFieldException("Required field \"format\" was filled bad")
 
         trailer = driver.find_element_by_name("trailer")
-        trailer.clear()
-        trailer.send_keys("http://www.ru")
-        trailer.send_keys(Keys.TAB)
+
+        self.sender(trailer, "http://www.ru")
 
         if str(trailer.get_attribute("class")) != "url":
             raise BadRequiredFieldException("Required field \"trailer\" was filled bad")
 
         driver.find_element_by_name("notes").clear()
         driver.find_element_by_name("notes").send_keys("Personal notes")
+
         driver.find_element_by_name("taglines").clear()
         driver.find_element_by_name("taglines").send_keys("Taglines")
+
         driver.find_element_by_name("plotoutline").clear()
         driver.find_element_by_name("plotoutline").send_keys("Plot outline")
+
         driver.find_element_by_name("plots").clear()
         driver.find_element_by_name("plots").send_keys("Plots")
+
         driver.find_element_by_id("text_languages_0").clear()
         driver.find_element_by_id("text_languages_0").send_keys("Russian")
+
         driver.find_element_by_name("subtitles").clear()
         driver.find_element_by_name("subtitles").send_keys("Subtitles")
+
         driver.find_element_by_name("audio").clear()
         driver.find_element_by_name("audio").send_keys("Audio")
+
         driver.find_element_by_name("video").clear()
         driver.find_element_by_name("video").send_keys("Video")
+
         driver.find_element_by_name("country").clear()
         driver.find_element_by_name("country").send_keys("Country")
+
         driver.find_element_by_name("genres").clear()
         driver.find_element_by_name("genres").send_keys("Genres")
+
         driver.find_element_by_name("director").clear()
         driver.find_element_by_name("director").send_keys("Director")
+
         driver.find_element_by_name("writer").clear()
         driver.find_element_by_name("writer").send_keys("Writer")
+
         driver.find_element_by_name("producer").clear()
         driver.find_element_by_name("producer").send_keys("Producer")
+
         driver.find_element_by_name("music").clear()
         driver.find_element_by_name("music").send_keys("Music")
+
         driver.find_element_by_name("cast").clear()
         driver.find_element_by_name("cast").send_keys("Cast")
-
-        # driver.find_element_by_name("imdbid").clear()
-        # film_id = driver.find_element_by_name("imdbid")
-        # film_id.send_keys("aaa")
-        # film_id.send_keys(Keys.TAB)
-
-        # color = film_id.value_of_css_property("background-color")
-        # print "Background color is: ", color
-        #
-        # color = film_id.value_of_css_property("border-color")
-        # print "Border color is: ", color
-        #
-        # color = film_id.value_of_css_property("font-color")
-        # print "Font color is: ", color
-        #
-        # all_input_elements = driver.find_elements_by_css_selector("input[type=\"text\"]")
-        #
-        # if all_input_elements > 0:
-        #     for element in all_input_elements:
-        #         #color = element.get_css_value("background-color")
-        #         #color = element.get_attribute("background-color")
-        #         color = element.value_of_css_property("background-color")
-        #         #if color == None: print "Color is None. It is OK"
-        #         #print color
-        # else:
-        #     print "We have not found input elements on page"
-        #     self.fail
 
         driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
         driver.find_element_by_link_text("Home").click()
 
         time.sleep(1)
-
-    def EXCLUDED_test_film_adding_corrupted_data(self):
-
-        driver = self.driver
-        driver.get(self.base_url + "/php4dvd/")
-
-        #login
-        driver.find_element_by_id("username").clear()
-        driver.find_element_by_id("username").send_keys("admin")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("admin")
-        driver.find_element_by_name("submit").click()
-
-
-        #trying to add film with corrupted data
-        driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_name("imdbid").clear()
-        driver.find_element_by_name("imdbid").send_keys("not a number")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_name("imdbid").clear()
-        driver.find_element_by_name("imdbid").send_keys("777")
-        driver.find_element_by_name("name").clear()
-        driver.find_element_by_name("name").send_keys("")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_name("name").clear()
-        driver.find_element_by_name("name").send_keys("Test Film Currupted Data")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_name("aka").clear()
-        driver.find_element_by_name("aka").send_keys("Scary Test Film Corrupted Data")
-        driver.find_element_by_name("year").clear()
-        driver.find_element_by_name("year").send_keys("not a number")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_name("year").clear()
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_name("year").clear()
-        driver.find_element_by_name("year").send_keys("-1")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_name("year").clear()
-        driver.find_element_by_name("year").send_keys("999999999999999999999999999999999999")
-        driver.find_element_by_name("duration").clear()
-        driver.find_element_by_name("duration").send_keys("not a number")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_name("duration").clear()
-        driver.find_element_by_name("duration").send_keys("13")
-        driver.find_element_by_id("formats").clear()
-        driver.find_element_by_id("formats").send_keys("")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("submit").click()
-        driver.find_element_by_id("formats").clear()
-        driver.find_element_by_id("formats").send_keys("VHS")
-        driver.find_element_by_id("loaned_yes").click()
-        driver.find_element_by_name("loandate").clear()
-        driver.find_element_by_name("loandate").send_keys("")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_link_text("Home").click()
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
--- php4dvd/test_film_removing.py
+++ php4dvd/test_film_removing.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import *
 import unittest, time, re
 
 class Removing(unittest.TestCase):
@@ -15,56 +17,14 @@ class Removing(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_removing_film_description(self):
-        driver = self.driver
-        driver.get(self.base_url + "/php4dvd/")
-
-        #login
-        driver.find_element_by_id("username").clear()
-        driver.find_element_by_id("username").send_keys("admin")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("admin")
-        driver.find_element_by_name("submit").click()
-
-        driver.find_element_by_class_name("movie_box").click()
-        driver.find_element_by_css_selector("img[alt=\"Edit\"]").click()
-
-        driver.find_element_by_name("imdbid").clear()
-        driver.find_element_by_name("name").clear()
-        driver.find_element_by_name("year").clear()
-        driver.find_element_by_name("duration").clear()
-        driver.find_element_by_name("rating").clear()
-        driver.find_element_by_id("formats").clear()
-        driver.find_element_by_id("text_languages_0").clear()
-
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_name("name").send_keys("Title")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_name("year").send_keys("not a number")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_name("year").clear()
-        driver.find_element_by_name("year").send_keys("-1")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_name("year").clear()
-        driver.find_element_by_name("year").send_keys("0")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_name("duration").clear()
-        driver.find_element_by_name("duration").send_keys("-10")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_name("duration").clear()
-        driver.find_element_by_name("duration").send_keys("13")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_name("rating").clear()
-        driver.find_element_by_name("rating").send_keys("-1")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_id("formats").clear()
-        driver.find_element_by_id("formats").send_keys("VHS")
-        driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
-        driver.find_element_by_link_text("Home").click()
-
     def test_film_removing(self):
+
+        class SmthWentWrongException(Exception):pass
+
         driver = self.driver
         driver.get(self.base_url + "/php4dvd/")
+
+        wait = WebDriverWait(self.driver, 3)
 
         #login
         driver.find_element_by_id("username").clear()
@@ -72,10 +32,19 @@ class Removing(unittest.TestCase):
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys("admin")
         driver.find_element_by_name("submit").click()
+
+        films_before_remove = driver.find_elements_by_class_name("title")
 
         driver.find_element_by_class_name("movie_box").click()
         driver.find_element_by_css_selector("img[alt=\"Remove\"]").click()
         driver.switch_to.alert.accept()
+
+        wait.until(visibility_of_element_located((By.CLASS_NAME, "movie_box")))
+
+        films_after_remove = driver.find_elements_by_class_name("title")
+
+        if films_before_remove == films_after_remove:
+            raise SmthWentWrongException("films_before_remove list == films_after_remove list")
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
