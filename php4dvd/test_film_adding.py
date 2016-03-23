185b320fb52d10a39ab3be773297200bfe76eb0e
semenkravchenko <semen.kravchenko@gmail.com>
2016-03-23 11:33:40 +0300

    Added some verifications in film adding test. Some tests were temporary excluded from scenario

--- php4dvd/test_film_adding.py
+++ php4dvd/test_film_adding.py
@@ -15,7 +15,7 @@ class Adding(unittest.TestCase):
         self.verificationErrors = []
         self.accept_next_alert = True
 
-    def test_film_adding_imdb_search(self):
+    def EXCLUDED_test_film_adding_imdb_search(self):
 
         search_target_string = u"Криминальное чтиво"
 
@@ -45,8 +45,11 @@ class Adding(unittest.TestCase):
 
     def test_film_adding_full_data(self):
 
+        class BadRequiredFieldException(Exception):pass
+
         driver = self.driver
         driver.get(self.base_url + "/php4dvd/")
+        driver.implicitly_wait(3)
 
         #login
         driver.find_element_by_id("username").clear()
@@ -57,18 +60,63 @@ class Adding(unittest.TestCase):
 
         #film adding
         driver.find_element_by_css_selector("img[alt=\"Add movie\"]").click()
-        driver.find_element_by_name("imdbid").clear()
-        driver.find_element_by_name("imdbid").send_keys("666")
-        driver.find_element_by_name("name").clear()
-        driver.find_element_by_name("name").send_keys("Test Film Full Data")
-        driver.find_element_by_name("aka").clear()
-        driver.find_element_by_name("aka").send_keys("Scary Test Film Full Data")
-        driver.find_element_by_name("year").clear()
-        driver.find_element_by_name("year").send_keys("1937")
-        driver.find_element_by_name("duration").clear()
-        driver.find_element_by_name("duration").send_keys("13")
-        driver.find_element_by_name("rating").clear()
-        driver.find_element_by_name("rating").send_keys("777")
+
+        imbdid = driver.find_element_by_name("imdbid")
+        imbdid.clear()
+        imbdid.send_keys("666")
+        imbdid.send_keys(Keys.TAB)
+
+        if str(imbdid.get_attribute("class")) != "digits":
+            raise BadRequiredFieldException("Required field \"imbdid\" was filled bad")
+
+        name = driver.find_element_by_name("name")
+        name.clear()
+        name.send_keys("Test Film Full Data")
+
+        aka = driver.find_element_by_name("aka")
+        aka.clear()
+        aka.send_keys("Scary Test Film Full Data")
+
+        year = driver.find_element_by_name("year")
+        year.clear()
+        year.send_keys("1937")
+        year.send_keys(Keys.TAB)
+
+        if str(year.get_attribute("class")) != "required digits":
+            raise BadRequiredFieldException("Required field \"year\" was filled bad")
+
+        duration = driver.find_element_by_name("duration")
+        duration.clear()
+        duration.send_keys("13")
+        duration.send_keys(Keys.TAB)
+
+        if str(duration.get_attribute("class")) != "digits":
+            raise BadRequiredFieldException("Required field \"duration\" was filled bad")
+
+        rating = driver.find_element_by_name("rating")
+        rating.clear()
+        rating.send_keys("777")
+        rating.send_keys(Keys.TAB)
+
+        if str(rating.get_attribute("class")) != "number":
+            raise BadRequiredFieldException("Required field \"rating\" was filled bad")
+
+        format = driver.find_element_by_name("format")
+        format.clear()
+        format.send_keys("VHS")
+        format.send_keys(Keys.TAB)
+
+        if str(format.get_attribute("class")) != "required ui-autocomplete-input":
+            raise BadRequiredFieldException("Required field \"format\" was filled bad")
+
+        trailer = driver.find_element_by_name("trailer")
+        trailer.clear()
+        trailer.send_keys("http://www.ru")
+        trailer.send_keys(Keys.TAB)
+
+        if str(trailer.get_attribute("class")) != "url":
+            raise BadRequiredFieldException("Required field \"trailer\" was filled bad")
+
         driver.find_element_by_name("notes").clear()
         driver.find_element_by_name("notes").send_keys("Personal notes")
         driver.find_element_by_name("taglines").clear()
@@ -99,12 +147,40 @@ class Adding(unittest.TestCase):
         driver.find_element_by_name("music").send_keys("Music")
         driver.find_element_by_name("cast").clear()
         driver.find_element_by_name("cast").send_keys("Cast")
+
+        # driver.find_element_by_name("imdbid").clear()
+        # film_id = driver.find_element_by_name("imdbid")
+        # film_id.send_keys("aaa")
+        # film_id.send_keys(Keys.TAB)
+
+        # color = film_id.value_of_css_property("background-color")
+        # print "Background color is: ", color
+        #
+        # color = film_id.value_of_css_property("border-color")
+        # print "Border color is: ", color
+        #
+        # color = film_id.value_of_css_property("font-color")
+        # print "Font color is: ", color
+        #
+        # all_input_elements = driver.find_elements_by_css_selector("input[type=\"text\"]")
+        #
+        # if all_input_elements > 0:
+        #     for element in all_input_elements:
+        #         #color = element.get_css_value("background-color")
+        #         #color = element.get_attribute("background-color")
+        #         color = element.value_of_css_property("background-color")
+        #         #if color == None: print "Color is None. It is OK"
+        #         #print color
+        # else:
+        #     print "We have not found input elements on page"
+        #     self.fail
+
         driver.find_element_by_css_selector("img[alt=\"Save\"]").click()
         driver.find_element_by_link_text("Home").click()
 
         time.sleep(1)
 
-    def test_film_adding_corrupted_data(self):
+    def EXCLUDED_test_film_adding_corrupted_data(self):
 
         driver = self.driver
         driver.get(self.base_url + "/php4dvd/")
